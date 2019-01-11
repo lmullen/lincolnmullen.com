342d1b4ab516566cd3bdf23e0c67063c05deea29
Lincoln Mullen <lincoln@lincolnmullen.com>
2019-01-11 13:41:43 -0500

    Better creation of new posts

---
+++ archetypes/blog.md
@@ -0,0 +1,8 @@
+---
+title: '{{ slicestr .Name 11 | humanize }}'
+date: {{ .Date }}
+slug: '{{ slicestr .Name 11 }}'
+series:
+subjects:
+draft: true
+---
---
+++ new-post.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# Make a new post and open it in VS Code
+
+if [ -z "$1" ]
+then
+  echo "Provide a slug."
+  exit 1
+fi
+
+date=`date "+%Y-%m-%d"`
+name=$1
+dir="content/blog/$date-$name"
+# Use Hugo archetypes to do the heavy lifting in creating the new file
+hugo new "blog/$date-$name.md"
+# Open the resulting file in VS Code
+code "content/blog/$date-$name.md"
+
